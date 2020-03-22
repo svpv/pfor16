a4843114b238d8986bc391e2f7820778c24b9aad
Alexey Tourbin <alexey.tourbin@gmail.com>
2020-03-22 05:45:32 +0300

    patch.h: special case fusing
    I've made a more elaborate benchmark, and have change my mind.
    The loop with an extra branch cannot run faster after all. :)
    
    So they are different loops now, and I optimize the data layout
    differently.  Without fusion, x0 and x1 should be loaded first.
    Otherwise i0 and i1 should be compared first.  Further if i0==i1,
    the value can be simply clobbered (i.e. we can provide the final
    16-bit value instead of an addend).

--- patch.h
+++ patch.h
@@ -5,7 +5,7 @@
 
 // Patch a block of 256 m-bit integers by applying n corrections from s,
 // in the form of <index, extra high bits> tuples.
-static inline bool patch256(const uint8_t *s, intptr_t n, uint16_t *v, int m)
+static inline bool patch256(const uint8_t *s, intptr_t n, uint16_t *v, int m, bool fuse)
 {
     if (--n <= 0) {
 	if (unlikely(n < 0))
@@ -14,22 +14,40 @@ static inline bool patch256(const uint8_t *s, intptr_t n, uint16_t *v, int m)
     }
     const uint8_t *last = s + 2 * n;
     unsigned i0, x0, i1, x1;
-    do {
-	i0 = s[0];
-	x0 = s[1];
-	i1 = s[2];
-	x1 = s[3];
-	s += 4;
-	if (unlikely(i0 == i1))
-	    v[i0] |= x0 << m | x1 << (m + 8);
-	else {
+    if (!fuse)
+	do {
+	    x0 = s[0];
+	    x1 = s[1];
+	    i0 = s[2];
+	    i1 = s[3];
+	    s += 4;
 	    v[i0] += x0 << m;
 	    v[i1] += x1 << m;
-	}
-    } while (s < last);
+	} while (s < last);
+    else
+	do {
+	    i0 = s[0];
+	    i1 = s[1];
+	    if (unlikely(i0 == i1)) {
+		uint16_t x;
+		memcpy(&x, s + 2, 2);
+		s += 4;
+#if defined(__GNUC__) && __BYTE_ORDER__ != __ORDER_LITTLE_ENDIAN__
+		x = __builtin_bswap16(x);
+#endif
+		v[i0] = x;
+	    }
+	    else {
+		x0 = s[2];
+		x1 = s[3];
+		s += 4;
+		v[i0] += x0 << m;
+		v[i1] += x1 << m;
+	    }
+	} while (s < last);
     if (s == last) {
-last:	i0 = s[0];
-	x0 = s[1];
+last:	x0 = s[0];
+	i0 = s[1];
 	v[i0] += x0 << m;
     }
     return true;
