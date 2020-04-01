0da11fabe728226312cb2133fc3f45a1ec42be8a
Alexey Tourbin <alexey.tourbin@gmail.com>
2020-04-01 12:32:31 +0300

    bitpack16.h bitunpack16.h: initial revision, 5-bit packing
    These files contain macros which describe how to pack and unpack
    5-bit integers in abstract terms, as if using only one 16-bit lane.
    We generate the packing and unpacking routines by cross-breeding
    the macros with SIMD engines (in C++ terms, the engine is a template
    parameter).  For example, to obtain bitpack16_5x64 and bitunpack16_5x64
    routines, we combine BitPack16_5x8 and BitUnpack16_5x8 macros with
    the B16x8 SIMD engine.
    
    The smallest size we are able to go down is 5x8, by combining 5x4 with
    B16x2.  This size is the smallest possible (40 bits = 5 bytes).  Note
    that the 5x4 macro uses quarter-load, unlike the 5x8 macro which
    requires only half-load.  This means that 5x4 cannot be combined with
    B16x8 and B16x16 which do not support quarter-loads.

---
+++ bitpack16.h
@@ -0,0 +1,102 @@
+// Copyright (c) 2020 Alexey Tourbin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+#include <assert.h>
+#include "bitpack16-simd.h"
+
+#define BitPack16_5x4(B, v, p)						\
+    do {								\
+	B##t x, y;							\
+	B##m m = B##mask(5);						\
+	x = B##and(B##load(v, 0), m);					\
+	x = B##or(x, B##shl(B##and(B##load(v, 1), m), 5));		\
+	x = B##or(x, B##shl(B##and(B##load(v, 2), m), 10));		\
+	y = B##load(v, 3);						\
+	x = B##or(x, B##shl(B##clean(y, B##mask(1)), 15));		\
+	B##store(p, 0, x);						\
+	y = B##extract(y, 1, 4, m);					\
+	B##quarterstore(p, 1, y);					\
+    } while (0)
+
+#define BitPack16_5x8(B, v, p)						\
+    do {								\
+	B##t x, y;							\
+	B##m m = B##mask(5);						\
+	x = B##and(B##load(v, 0), m);					\
+	x = B##or(x, B##shl(B##and(B##load(v, 1), m), 5));		\
+	x = B##or(x, B##shl(B##and(B##load(v, 2), m), 10));		\
+	y = B##load(v, 3);						\
+	x = B##or(x, B##shl(B##clean(y, B##mask(1)), 15));		\
+	B##store(p, 0, x);						\
+	y = B##extract(y, 1, 4, m);					\
+	y = B##or(y, B##shl(B##and(B##load(v, 4), m), 4));		\
+	y = B##or(y, B##shl(B##and(B##load(v, 5), m), 9));		\
+	x = B##load(v, 6);						\
+	y = B##or(y, B##shl(B##clean(x, B##mask(2)), 14));		\
+	B##store(p, 1, y);						\
+	x = B##extract(x, 2, 3, m);					\
+	x = B##or(x, B##shl(B##and(B##load(v, 7), m), 3));		\
+	B##halfstore(p, 2, x);						\
+    } while (0)
+
+#define BitPack16_5x16(B, v, p)						\
+    do {								\
+	B##t x, y;							\
+	B##m m = B##mask(5);						\
+	x = B##and(B##load(v, 0), m);					\
+	x = B##or(x, B##shl(B##and(B##load(v, 1), m), 5));		\
+	x = B##or(x, B##shl(B##and(B##load(v, 2), m), 10));		\
+	y = B##load(v, 3);						\
+	x = B##or(x, B##shl(B##clean(y, B##mask(1)), 15));		\
+	B##store(p, 0, x);						\
+	y = B##extract(y, 1, 4, m);					\
+	y = B##or(y, B##shl(B##and(B##load(v, 4), m), 4));		\
+	y = B##or(y, B##shl(B##and(B##load(v, 5), m), 9));		\
+	x = B##load(v, 6);						\
+	y = B##or(y, B##shl(B##clean(x, B##mask(2)), 14));		\
+	B##store(p, 1, y);						\
+	x = B##extract(x, 2, 3, m);					\
+	x = B##or(x, B##shl(B##and(B##load(v, 7), m), 3));		\
+	x = B##or(x, B##shl(B##and(B##load(v, 8), m), 8));		\
+	y = B##load(v, 9);						\
+	x = B##or(x, B##shl(B##clean(y, B##mask(3)), 13));		\
+	B##store(p, 2, x);						\
+	y = B##extract(y, 3, 2, m);					\
+	y = B##or(y, B##shl(B##and(B##load(v, 10), m), 2));		\
+	y = B##or(y, B##shl(B##and(B##load(v, 11), m), 7));		\
+	x = B##load(v, 12);						\
+	y = B##or(y, B##shl(B##clean(x, B##mask(4)), 12));		\
+	B##store(p, 3, y);						\
+	x = B##extract(x, 4, 1, m);					\
+	x = B##or(x, B##shl(B##and(B##load(v, 13), m), 1));		\
+	x = B##or(x, B##shl(B##and(B##load(v, 14), m), 6));		\
+	x = B##or(x, B##shl(B##clean(B##load(v, 15), m), 11));		\
+	B##store(p, 4, x);						\
+    } while (0)
+
+#define BitPack16_func(m, n, X, N)					\
+    static inline void bitpack16_##m##x##N(const uint16_t *v, void *p)	\
+    {									\
+	static_assert(n * X == N, "");					\
+	BitPack16_##m##x##n(B16x##X, v, p);				\
+    }
+#include "bitpack16-list.h"
+#undef BitPack16_func
---
+++ bitunpack16.h
@@ -0,0 +1,87 @@
+// Copyright (c) 2020 Alexey Tourbin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+#include "bitpack16-simd.h"
+
+#define BitUnpack16_5x4(B, v, p)					\
+    do {								\
+	B##t x, y;							\
+	B##m m = B##mask(5);						\
+	x = B##load(p, 0);						\
+	B##store(v, 0, B##and(x, m));					\
+	B##store(v, 1, B##and(B##shr(x, 5), m));			\
+	B##store(v, 2, B##and(B##shr(x, 10), m));			\
+	y = B##quarterload(p, 1);					\
+	x = B##clean(B##shr(x, 15), B##mask(1));			\
+	B##store(v, 3, B##or(x, B##shl(y, 1)));				\
+    } while (0)
+
+#define BitUnpack16_5x8(B, v, p)					\
+    do {								\
+	B##t x, y;							\
+	B##m m = B##mask(5);						\
+	x = B##load(p, 0);						\
+	B##store(v, 0, B##and(x, m));					\
+	B##store(v, 1, B##and(B##shr(x, 5), m));			\
+	B##store(v, 2, B##and(B##shr(x, 10), m));			\
+	y = B##load(p, 1);						\
+	B##store(v, 3, B##combine(x, y, 1, 4, m));			\
+	B##store(v, 4, B##and(B##shr(y, 4), m));			\
+	B##store(v, 5, B##and(B##shr(y, 9), m));			\
+	x = B##halfload(p, 2);						\
+	B##store(v, 6, B##combine(y, x, 2, 3, m));			\
+	B##store(v, 7, B##and(B##shr(x, 3), m));			\
+    } while (0)
+
+#define BitUnpack16_5x16(B, v, p)					\
+    do {								\
+	B##t x, y;							\
+	B##m m = B##mask(5);						\
+	x = B##load(p, 0);						\
+	B##store(v, 0, B##and(x, m));					\
+	B##store(v, 1, B##and(B##shr(x, 5), m));			\
+	B##store(v, 2, B##and(B##shr(x, 10), m));			\
+	y = B##load(p, 1);						\
+	B##store(v, 3, B##combine(x, y, 1, 4, m));			\
+	B##store(v, 4, B##and(B##shr(y, 4), m));			\
+	B##store(v, 5, B##and(B##shr(y, 9), m));			\
+	x = B##load(p, 2);						\
+	B##store(v, 6, B##combine(y, x, 2, 3, m));			\
+	B##store(v, 7, B##and(B##shr(x, 3), m));			\
+	B##store(v, 8, B##and(B##shr(x, 8), m));			\
+	y = B##load(p, 3);						\
+	B##store(v, 9, B##combine(x, y, 3, 2, m));			\
+	B##store(v, 10, B##and(B##shr(y, 2), m));			\
+	B##store(v, 11, B##and(B##shr(y, 7), m));			\
+	x = B##load(p, 4);						\
+	B##store(v, 12, B##combine(y, x, 4, 1, m));			\
+	B##store(v, 13, B##and(B##shr(x, 1), m));			\
+	B##store(v, 14, B##and(B##shr(x, 6), m));			\
+	B##store(v, 15, B##clean(B##shr(x, 11), m));			\
+    } while (0)
+
+#define BitPack16_func(m, n, X, N)					\
+    static inline void bitunpack16_##m##x##N(uint16_t *v, const void *p)\
+    {									\
+	BitUnpack16_##m##x##n(B16x##X, v, p);				\
+    }
+#include "bitpack16-list.h"
+#undef BitPack16_func
