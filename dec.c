83da274144d2940d4ff354537bb62392e58800ec
Alexey Tourbin <alexey.tourbin@gmail.com>
2020-09-07 04:44:20 +0300

    trying to extend x256 e0=0..46 e1=0..31
    Update 2021: I've just reviewed the scheme and it makes sense.  We have
    roughly (m=0..16)x(N=64,128,25) > 48 distinct types of blocks as indicated
    by bctl (the leading byte).  The best we can do to dispatch the cases is
    to switch on 6 bits of bctl.  The compiler will generate a jump table
    which is roughly 64*4 bytes or 4 cache lines.  (If we switched on every
    possible bctl value, we would squander 16 cache lines.)
    
    Hence we switch on 6 MSB bits while 2 LSB bits convey extra information:
    
        [765432|1|0]
              ^ ^ ^
    	  | | |
    	  | | `- has pctl + patch
    	  | `--- has a clobber entry / outstanding tuple
              `----- block type (m=0..16)x(N=64,128,256)
    
    The whole block looks like this:
    
        [bctl] [blob] [i|w] [pctl] [patch]
                   ^     ^              ^
    	       |     |              |
    	       |     |              `- packed patch tuples
                   |     `---------------- clobber entry / outstanding tuple
                   `---------------------- packed m-bit integers
    
    The pctl byte specifies two nibbles: e0 for the unrolled loop with
    small/paired MSB quantities, and e1 bigger MSB quantities.  But the
    nibble values are limited to 15, which is not quite enough for N=256.
    Therefore, for some N=256 blocks we allocate two cases in the dispatch:
    
        case C + 0:
        case C + 1:
    	    bitunpack16_##M##x256(v, src);
    
    so that we can take advantage of bit #2.  LSB bits in bctl are then
    interpreted differently:
    
        [76543|2|10]
               ^  ^
    	   |  |
    	   |  `- has pctl + patch
    	   `---- has a clobber entry / outstanding tuple
    
    If (bctl & 3) == 0, there is no pctl + patch.  Otherwise, we have pctl +
    patch and extra 3 possible values: 0b01, 0b10, or 0b11.  We use them to
    extend the pctl byte: the range of e0 is now 0..23(x2), and e1=0..31.
    
    When m >= 8 (with N=256), there is no need for a 16-bit clobber entry:
    an 8-bit entry from e1 is enough to make up for any value.  Hence we can
    reuse bit #1 to provide extra state, without allocating two cases in the
    dispatch:
    
        [765432|10]
                 ^
    	     |
    	     `- has pctl + patch
    
    With N=64 and N=128, we do not extend the ranges of e0 and e1.
    Hence with m >= 6 resp. m >= 7, bit #1 remains unused.
    
    Finally, when m >= 12 (with N=256), 4-bit paired quantities are always
    enough to make up for any value, so we pass e1=0 and expect the compiler
    to optimize out the e1 loop.  The only gotcha is that the entries are
    paired, but we may have an odd number of them.  So we reuse the
    outstanding entry (also called the "f tuple", not sure why): bit #1
    indicates that the odd entry is encoded with 2 bytes (unlike "full
    clobber" entries which are 3 bytes).

--- dec.c
+++ dec.c
@@ -1,6 +1,9 @@
 #include <assert.h>
 #include "bitunpack16.h"
 
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(x, 0)
+
 #define Mask(k) ((1U<<(k))-1)
 
 static inline const unsigned char *patch64(const unsigned char *src, uint16_t *v,
@@ -79,73 +82,138 @@ const unsigned char *dec64(const unsigned char *src, size_t len, uint16_t *v, si
     uint16_t *last64 = v + n - 64;
     do {
 	unsigned bctl = *src++;
-	switch (bctl & Mask(6)) {
-#define Case64(M)						\
-	case 3 * M + 0:						\
-	    bitunpack16_##M##x64(v, src);			\
-	    src += M * 64 / 8;					\
-	    if (bctl & 0x80) {					\
-		unsigned i = src[0] & Mask(6);			\
+	switch (bctl >> 2) {
+	// with 3-byte f tuple, for small m
+#define CaseC1F3(C, M, N)					\
+	case C:							\
+	    bitunpack16_##M##x##N(v, src);			\
+	    src += M * N / 8;					\
+	    if (unlikely(bctl & 2)) {				\
+		unsigned i = src[0] & (N - 1);			\
 		unsigned x = Bload16le(src + 1);		\
 		v[i] = x, src += 3;				\
 	    }							\
-	    if (bctl & 0x40) {					\
+	    if (likely(bctl & 1)) {				\
 		unsigned pctl = *src++;				\
 		unsigned e0 = pctl & 15, e1 = pctl >> 4;	\
-		src = patch64(src, v, M, e0, e1);		\
+		src = patch##N(src, v, M, e0, e1);		\
 	    }							\
-	    v += 64;						\
+	    v += N;						\
 	    break
-#define Case128(M)						\
-	case 3 * M + 1:						\
-	    bitunpack16_##M##x128(v, src);			\
-	    src += M * 128 / 8;					\
-	    if (bctl & 0x80) {					\
-		unsigned i = src[0] & Mask(7);			\
-		unsigned x = Bload16le(src + 1);		\
-		v[i] = x, src += 3;				\
-	    }							\
-	    if (bctl & 0x40) {					\
+	// without f tuple, for medium m
+#define CaseC1F0(C, M, N)					\
+	case C:							\
+	    bitunpack16_##M##x##N(v, src);			\
+	    src += M * N / 8;					\
+	    if (likely(bctl & 1)) {				\
 		unsigned pctl = *src++;				\
 		unsigned e0 = pctl & 15, e1 = pctl >> 4;	\
-		src = patch128(src, v, M, e0, e1);		\
+		src = patch##N(src, v, M, e0, e1);		\
 	    }							\
-	    v += 128;						\
+	    v += N;						\
 	    break
-#define Case256(M)						\
-	case 3 * M + 2:						\
+	// with 2-byte f tuple (e1=0), for big m
+#define CaseC1F2(C, M, N)					\
+	case C:							\
+	    bitunpack16_##M##x##N(v, src);			\
+	    src += M * N / 8;					\
+	    if (bctl & 2) {					\
+		unsigned w = Bload16le(src);			\
+		unsigned i = src[0] & (N - 1);			\
+		unsigned x = w >> (N == 64 ? 6 : 7);		\
+		v[i] += (x << M), src += 2;			\
+	    }							\
+	    if (likely(bctl & 1)) {				\
+		unsigned pctl = *src++;				\
+		unsigned e0 = pctl & 15;			\
+		src = patch##N(src, v, M, e0, 0);		\
+	    }							\
+	    v += N;						\
+	    break
+	// extended pctl, with 3-byte f tuple, for N=256 m=0..7, 8 states:
+	// f=0 p=00,01,10,11
+	// f=1 p=00,01,10,11
+#define CaseC2F3_256(C, M)					\
+	case C + 0:						\
+	case C + 1:						\
 	    bitunpack16_##M##x256(v, src);			\
 	    src += M * 256 / 8;					\
-	    if (bctl & 0x80) {					\
+	    if (unlikely(bctl & 4)) {				\
 		unsigned i = src[0];				\
 		unsigned x = Bload16le(src + 1);		\
 		v[i] = x, src += 3;				\
 	    }							\
-	    if (bctl & 0x40) {					\
-		unsigned pctl = *src++;				\
-		unsigned e0 = pctl & 15, e1 = pctl >> 4;	\
+	    if (likely(bctl & 3)) {				\
+		unsigned pex = (bctl & 3) - 1;			\
+		unsigned pctl = *src++ | pex << 8;		\
+		unsigned e0 = pctl >> 5, e1 = pctl & 31;	\
 		src = patch256(src, v, M, e0, e1);		\
 	    }							\
 	    v += 256;						\
 	    break
-#define Case(M) Case64(M); Case128(M); Case256(M)
-	Case(0);
-	Case(1);
-	Case(2);
-	Case(3);
-	Case(4);
-	Case(5);
-	Case(6);
-	Case(7);
-	Case(8);
-	Case(9);
-	Case(10);
-	Case(11);
-	Case(12);
-	Case(13);
-	Case(14);
-	Case(15);
-	Case(16);
+	// extended pctl, without f tuple, for N=256 m=8..11
+#define CaseC1F0_256(C, M)					\
+	case C:							\
+	    bitunpack16_##M##x256(v, src);			\
+	    src += M * 256 / 8;					\
+	    if (likely(bctl & 3)) {				\
+		unsigned pex = (bctl & 3) - 1;			\
+		unsigned pctl = *src++ | pex << 8;		\
+		unsigned e0 = pctl >> 5, e1 = pctl & 31;	\
+		src = patch256(src, v, M, e0, e1);		\
+	    }							\
+	    v += 256;						\
+	    break
+	// with 2-byte f tuple (e1=0), for N=256 m=12..15
+#define CaseC1F2_256(C, M)					\
+	case C:							\
+	    bitunpack16_##M##x256(v, src);			\
+	    src += M * 256 / 8;					\
+	    if (bctl & 2) {					\
+		unsigned i = src[0];				\
+		unsigned x = src[1];				\
+		v[i] += (x << M), src += 2;			\
+	    }							\
+	    if (likely(bctl & 1)) {				\
+		unsigned pctl = *src++;				\
+		unsigned e0 = pctl & 31;			\
+		src = patch256(src, v, M, e0, 0);		\
+	    }							\
+	    v += 256;						\
+	    break
+
+	CaseC1F3( 0,  0, 64); CaseC1F3( 1,  0, 128); CaseC2F3_256( 2,  0);
+	CaseC1F3( 4,  1, 64); CaseC1F3( 5,  1, 128); CaseC2F3_256( 6,  1);
+	CaseC1F3( 8,  2, 64); CaseC1F3( 9,  2, 128); CaseC2F3_256(10,  2);
+	CaseC1F3(12,  3, 64); CaseC1F3(13,  3, 128); CaseC2F3_256(14,  3);
+	CaseC1F3(16,  4, 64); CaseC1F3(17,  4, 128); CaseC2F3_256(18,  4);
+	CaseC1F3(20,  5, 64); CaseC1F3(21,  5, 128); CaseC2F3_256(22,  5);
+	CaseC1F0(24,  6, 64); CaseC1F3(25,  6, 128); CaseC2F3_256(26,  6);
+	CaseC1F0(28,  7, 64); CaseC1F0(29,  7, 128); CaseC2F3_256(30,  7);
+	CaseC1F0(32,  8, 64); CaseC1F0(33,  8, 128); CaseC1F0_256(34,  8);
+	CaseC1F0(35,  9, 64); CaseC1F0(36,  9, 128); CaseC1F0_256(37,  9);
+	CaseC1F2(38, 10, 64); CaseC1F0(39, 10, 128); CaseC1F0_256(40, 10);
+	CaseC1F2(41, 11, 64); CaseC1F2(42, 11, 128); CaseC1F0_256(43, 11);
+	CaseC1F2(44, 12, 64); CaseC1F2(45, 12, 128); CaseC1F2_256(46, 12);
+	CaseC1F2(47, 13, 64); CaseC1F2(48, 13, 128); CaseC1F2_256(49, 13);
+	CaseC1F2(50, 14, 64); CaseC1F2(51, 14, 128); CaseC1F2_256(52, 14);
+	CaseC1F2(53, 15, 64); CaseC1F2(54, 15, 128); CaseC1F2_256(55, 15);
+
+	case 56:
+	    bitunpack16_16x64(v, src);
+	    src += 16 * 64 / 8;
+	    v += 64;
+	    break;
+	case 57:
+	    bitunpack16_16x128(v, src);
+	    src += 16 * 128 / 8;
+	    v += 128;
+	    break;
+	case 58:
+	    bitunpack16_16x256(v, src);
+	    src += 16 * 256 / 8;
+	    v += 256;
+	    break;
 	default: assert(0);
 	}
     } while (v <= last64);
